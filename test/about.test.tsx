c54c1a08f1fb5a8a086d81f18eee88163ed71b74
Mikhail Myshakin <myshakinm@mail.ru>
2025-07-10 22:43:24 +0300

    added -> "jsx": "react-jsx",

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -1,10 +1,9 @@
 import { render } from "@testing-library/react";
-import { About } from "./../src/client/pages/About";
+import { About } from "../src/client/pages/About";
 
 describe("About", () => {
     it("Should render author name", () => {
         const { container } = render(<About />);
-
         expect(container.textContent).toBe("AboutAuthor: [Мышакин Михаил]");
     });
 });
