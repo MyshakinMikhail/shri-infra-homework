2b651e7578d2b6b2e1130518a61ee75a8dce489d
Анастасия <71327963+anasteisha-4@users.noreply.github.com>
2025-07-20 15:13:54 +0300

    Update About.tsx

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
             <div className="row">
                 <div className="col">
                     <h1>About</h1>
-                    <p>Author: Мышакин Михаил</p>
+                    <p>Author: test</p>
                 </div>
             </div>
         </div>
